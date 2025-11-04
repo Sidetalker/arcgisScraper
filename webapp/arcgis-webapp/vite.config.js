41581b6d7d59f98534d27c9ddb0643b2bdb209ee
Kevin Sullivan <kevin@sideapps.com>
2025-11-03 21:55:53 -0500

    dem touchups

---
+++ webapp/arcgis-webapp/vite.config.js
@@ -0,0 +1,18 @@
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vitest/config';
+const projectRootDir = fileURLToPath(new URL('.', import.meta.url));
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(projectRootDir, './src'),
+        },
+    },
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: './vitest.setup.ts',
+    },
+});
