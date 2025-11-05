b1620fd24ede0ab8e5fddddaea1cbdf6e7ecb9cf
Kevin Sullivan <kevin@sideapps.com>
2025-11-05 16:28:26 -0500

    fixy fix

--- webapp/arcgis-webapp/src/App.tsx
+++ webapp/arcgis-webapp/src/App.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useMemo, useState } from 'react';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 
 import './App.css';
